74c623e897a595ac60e9cedfaa9beccf10e95f8a
Thiago Marques <thiagomarques408@gmail.com>
2023-06-10 13:44:50 -0300

    feat: adiciona dropdown

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { TextInput, Button, Text, Card } from 'react-native-paper';
+import { Button, Text, Card } from 'react-native-paper';
 import { Feather } from '@expo/vector-icons';
 import Constants from 'expo-constants';
 import axios from "axios";
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const apiCredentials = {
-  key: '0c6eaa4026msh310d8c145e29a91p1fa6b6jsn3e580de12cc3',
-  host: 'open-weather13.p.rapidapi.com',
+  weather13: {
+    key: '',
+    host: 'open-weather13.p.rapidapi.com',
+  },
+  geoDB: {
+    key: '',
+    host: 'wft-geo-db.p.rapidapi.com',
+  }
 }
 
 export default function App() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState(null);
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [openDropDown, setOpenDropDown] = useState(false);
+  const [cityOptions, setCityOptions] = useState([]);
 
   function convertToCelsius(fahrenheit) {
     return (((fahrenheit - 32) * 5) / 9).toFixed(2);
@@ -21,12 +31,13 @@ export default function App() {
   async function fetchWeather() {
     if (!city) return;
 
+    setLoading(true);
     const encodedCity = encodeURIComponent(city);
     const options = {
       url: `https://open-weather13.p.rapidapi.com/city/${encodedCity}`,
       headers: {
-        "X-RapidAPI-Key": apiCredentials.key,
-        "X-RapidAPI-Host": apiCredentials.host,
+        "X-RapidAPI-Key": apiCredentials.weather13.key,
+        "X-RapidAPI-Host": apiCredentials.weather13.host,
       },
     };
 
@@ -39,8 +50,46 @@ export default function App() {
       }
 
       setWeather(weatherData);
+      setLoading(false);
     } catch (error) {
       console.error("Erro na chamada da API:", error);
+    }
+  }
+
+  function buildCityOptions(data) {
+    if (!data) return [];
+
+    const options = data.map(city => ({ label: city.name, value: city.city }));
+
+    return options.filter((option, index, self) =>
+      index === self.findIndex((t) => (
+        t.value === option.value
+      ))
+    )
+  }
+
+  async function searchCity(prefix) {
+    if (prefix.length < 3) return;
+
+    const encodedPrefix = encodeURIComponent(prefix);
+    const options = {
+      url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
+      params: {
+        types: 'city',
+        namePrefix: encodedPrefix
+      },
+      headers: {
+        'X-RapidAPI-Key': apiCredentials.geoDB.key,
+        'X-RapidAPI-Host': apiCredentials.geoDB.host,
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      const cities = buildCityOptions(response.data.data)
+
+      setCityOptions(cities);
+    } catch (_error) {
     }
   }
 
@@ -52,19 +101,27 @@ export default function App() {
       </View>
 
       <Card style={styles.card}>
-        <TextInput
-          label="Digite a região"
+        <DropDownPicker
+          open={openDropDown}
           value={city}
-          onChangeText={text => setCity(text)}
-          style={styles.input}
+          items={cityOptions}
+          setOpen={setOpenDropDown}
+          setValue={setCity}
+          searchable={true}
+          placeholder="Buscar cidade"
+          searchPlaceholder={"Digite pelo menos 3 caracteres"}
+          language="PT"
+          onChangeSearchText={searchCity}
+          onSelectItem={() => setWeather(null)}
+          maxHeight={300}
         />
-        <Button mode="contained" onPress={fetchWeather} style={styles.button}>
+
+        <Button mode="contained" onPress={fetchWeather} style={styles.button(openDropDown)} loading={loading}>
           Ver Previsão
         </Button>
 
         {weather && (
           <View style={styles.weatherContainer}>
-            <Text style={styles.weatherText}>Cidade: {city}</Text>
             <Text style={styles.weatherText}>Temperatura: {weather.temperature} °C</Text>
             <Text style={styles.weatherText}>Mínima: {weather.minTemperature} °C</Text>
             <Text style={styles.weatherText}>Máxima: {weather.maxTemperature} °C</Text>
@@ -96,17 +153,16 @@ const styles = StyleSheet.create({
     padding: 16,
     elevation: 4,
   },
-  input: {
+  button: (openDropDown) => ({
+    marginTop: 32,
     marginBottom: 16,
-  },
-  button: {
-    marginTop: 8,
-  },
+    display: openDropDown ? 'none' : 'flex',
+  }),
   weatherContainer: {
     marginTop: 16,
   },
   weatherText: {
     fontSize: 18,
     marginBottom: 8,
-  },
+  }
 });
